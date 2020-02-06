c79b8ba97fb084c3c06ed03fb77031de6c1df161
Emanuel <7004075+emanueltomasinborda@users.noreply.github.com>
2020-02-06 22:36:34 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ try {
 } catch (e) {
     ghostConfig = {
         production: {
-            apiUrl: process.env.GHOST_API_UR,
+            apiUrl: process.env.GHOST_API_URL,
             contentApiKey: process.env.GHOST_CONTENT_API_KEY,
         },
     }
