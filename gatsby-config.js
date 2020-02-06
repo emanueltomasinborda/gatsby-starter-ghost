39a8c8dbbb1f2f304a35e80bd4786ad766592326
Emanuel <7004075+emanueltomasinborda@users.noreply.github.com>
2020-02-06 22:15:26 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ try {
 } catch (e) {
     ghostConfig = {
         production: {
-            apiUrl: process.env.noche.eu.org,
+            apiUrl: process.env.GHOST_API_URL,
             contentApiKey: process.env.GHOST_CONTENT_API_KEY,
         },
     }
